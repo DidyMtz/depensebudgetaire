40f5350ed17809e17ce94148fe236ec85265de04
DidyMtz <102537566+DidyMtz@users.noreply.github.com>
2023-06-02 19:20:41 +0100

    size & offset

---
+++ depbudget1/app.js
@@ -0,0 +1,40 @@
+const reasonInput = document.querySelector("#reasoninput");
+const amountInput = document.querySelector("#amountinput");
+const deleteBtn = document.querySelector("#deletebtn");
+const confirmBtn = document.querySelector("#addbtn");
+const expensesList = document.querySelector("#renduForm");
+const totalExpensesOutput = document.querySelector("#totalExpenses");
+
+let totalExpenses = 0;
+
+const clear = () => {
+  reasonInput.value = "";
+  amountInput.value = "";
+};
+confirmBtn.addEventListener("click", () => {
+  const enteredReason = reasonInput.value;
+  const enteredAmount = amountInput.value;
+
+  //validation
+  if (
+    enteredReason.trim().length <= 0 ||
+    enteredAmount <= 0 ||
+    enteredAmount.trim().length <= 0
+  ) {
+    return;
+  }
+
+  const elementList = document.createElement("ion-item");
+  elementList.textContent = enteredReason + " : £" + enteredAmount;
+//remplir list
+  expensesList.appendChild(elementList);
+
+  //calcul total expenses
+  totalExpenses += +enteredAmount;
+  totalExpensesOutput.textContent = totalExpenses;
+
+  clear();
+});
+
+//reinitialiser formulaire
+deleteBtn.addEventListener("click", () => clear());
